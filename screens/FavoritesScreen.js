30e32ddf2ec87fb8aff86fb3bd1917a4de098305
Eric <ericmares13@gmail.com>
2019-11-06 13:00:31 +0100

    checking if thers fav

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { Item, HeaderButtons } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../components/HeaderButton';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 const FavoritesScreen = props => {
   const favMeals = useSelector(state => state.meals.favoriteMeals);
+
+  if (favMeals.length === 0 || !favMeals) {
+    <View style={styles.content}>
+      <DefaultText>No Fav meals</DefaultText>
+    </View>;
+  }
 
   return <MealList listData={favMeals} navigation={props.navigation} />;
 };
@@ -29,5 +37,13 @@ FavoritesScreen.navigationOptions = navData => {
     )
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default FavoritesScreen;
